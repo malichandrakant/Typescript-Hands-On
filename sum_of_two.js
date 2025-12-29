8201943e55fd565338d5a3b8011bfbc4f2fdd25b
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2025-12-29 18:28:09 +0530

    program sum of two numbers

---
+++ sum_of_two.js
@@ -0,0 +1,7 @@
+// function to calculate sum of two numbers
+function sumOfTwo(a, b) {
+    return a + b;
+}
+// calling the function
+var result = sumOfTwo(4, 6);
+console.log("The sum of two numbers is: " + result);
---
+++ sum_of_two.ts
@@ -0,0 +1,10 @@
+
+
+// function to calculate sum of two numbers
+function sumOfTwo(a:number,b:number) :number{
+    return a+b;
+}
+
+// calling the function
+let result = sumOfTwo(4,6);
+console.log("The sum of two numbers is: " + result);
