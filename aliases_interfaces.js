7a437ae443514aa2f7a83b6a98eebf73d9a5734c
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-01-01 16:37:11 +0530

    feat: practice TypeScript type aliases and interfaces

---
+++ aliases_interfaces.js
@@ -0,0 +1,10 @@
+var user = {
+    name: "Rahul",
+    age: 22
+};
+var user2 = {
+    name: "Anjali",
+    age: 25
+};
+console.log(user);
+console.log(user2);
---
+++ aliases_interfaces.ts
@@ -0,0 +1,24 @@
+type Person={
+    name:string;
+    age:number;
+}
+
+let user:Person={
+    name:"Rahul",
+    age:22
+};
+
+// interface
+
+interface Person2{
+    name:string;
+    age:number;
+}
+
+let user2:Person2={
+    name:"Anjali",
+    age:25
+};
+
+console.log(user);
+console.log(user2);
