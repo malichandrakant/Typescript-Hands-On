f3a87d5fa2ea413148ec191a7bcb6c04118262d5
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-01-07 11:40:38 +0530

    feat: add tuple examples in TypeScript

---
+++ tuple.js
@@ -0,0 +1,16 @@
+// let user:[number,string];
+// user=[1,"chandrakant"];
+// console.log(user);
+// let admin:[number,string];
+// admin=[2,"kumar"];
+// console.log(admin);
+// let student:[number,string,boolean];
+// student=[3,"rahul",true];
+// console.log(student);
+// Tuple with array
+var users = [
+    [1, "chandrakant"],
+    [2, "Amit"],
+    [3, "rahul"]
+];
+console.log(users);
---
+++ tuple.ts
@@ -0,0 +1,34 @@
+// let user:[number,string];
+
+// user=[1,"chandrakant"];
+// console.log(user);
+
+
+// let admin:[number,string];
+// admin=[2,"kumar"];
+// console.log(admin);
+
+
+// let student:[number,string,boolean];
+// student=[3,"rahul",true];
+// console.log(student);
+
+
+
+// Tuple with array
+
+// let users:[number,string][]=[
+//     [1,"chandrakant"],
+//     [2,"Amit"],
+//     [3,"rahul"]
+// ];
+
+// console.log(users);
+
+
+// Named Tuple
+
+let employee:[id:number,name:string
+]=[1,"chandrakant"];
+console.log(employee);
+
